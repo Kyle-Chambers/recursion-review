fba74217842cc5ad4f494231ff1bd0ed213be594
Kyle-Chambers <kylejchambers@gmail.com>
2018-01-15 20:35:11 -0800

    Completed req's to get tests passing

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,27 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+
+var getElementsByClassName = function(className) /*targetClassName */{
+  var result = [];
+
+  (function traverseDOM (node) {
+    var c;
+    !node.classList ? c = "" : c = node.classList.value;
+
+    if (c.indexOf(className) > -1){
+      result.push(node);
+    }
+
+    var children = node.childNodes
+    if (children){
+      for (var i = 0; i < children.length; i++)
+      traverseDOM(children[i]);
+    }
+  })(document.body);
+
+  return result;
 };
+// document.body
+// element.childNodes
+// element.classList
